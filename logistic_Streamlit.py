05f8c22fa168ed9ef3e0a398c5010668397e4cf1
vino469 <vinothnambi06@gmail.com>
2026-01-07 19:42:57 +0530

    Update logistic_Streamlit.py

--- logistic_Streamlit.py
+++ logistic_Streamlit.py
@@ -1,7 +1,12 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+import joblib
+
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
 
 st.title("🚢 Titanic Survival Prediction")
 
